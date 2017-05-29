d1caa150b538d32b12cfbf86564bfb9516b1fcee
christianJCK <jecko.christian@gmail.com>
2017-05-29 14:04:56 +0200

    added layout for first test of form

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -4,13 +4,20 @@
 namespace AppBundle\Controller;
 
 
+use AppBundle\Entity\Contact;
+use AppBundle\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class MainController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('app/main/index.html.twig');
+
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        return $this->render('app/main/index.html.twig', array(
+            'form' => $form->createView()
+        ));
     }
 
 
