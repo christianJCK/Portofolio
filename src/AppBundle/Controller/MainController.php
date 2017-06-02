7d8383333935433bbfbb0a0ee1a9cde5ce85bf70
christianJCK <jecko.christian@gmail.com>
2017-06-02 10:46:55 +0200

    form done

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -30,15 +30,16 @@ class MainController extends Controller
                 ->setTo('goteki85@gmail.com')
                 ->setBody($data->getMail() . ' ' . $data->getMessage());
 
-            //$this->get('mailer')->send($message);
+            $this->get('mailer')->send($message);
 
-            $this->addFlash('notice', "<strong>Message bien envoyé !</strong><br> J'essaierais d'y répondre le plus rapidement possible.<br>
+            $this->addFlash('notice', "<strong>MESSAGE ENVOYÉ !</strong><br> J'essaierais d'y répondre le plus rapidement possible.<br>
              En attendant vous pouvez consulter cette délicieuse <a href='http://www.marmiton.org/recettes/recette_pancakes-a-la-banane_45575.aspx' target='_blank'>recette</a> de pancake. À bientôt !");
+
+            return $this->redirectToRoute('app_homepage');
         }
 
         return $this->render('app/main/index.html.twig', array(
             'form' => $formContact->createView(),
-
         ));
     }
 }
